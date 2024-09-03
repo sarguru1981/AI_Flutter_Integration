4dd7d6673ffc067637b5b5ebb50bd2d16085a758
Sarguru Subramanian <sarguru1981@gmail.com>
2024-09-03 18:38:11 +0530

    Implemented the Enhanced search logic using Gemini AI

--- ai_flutter_search_functionality/lib/food_list_page.dart
+++ ai_flutter_search_functionality/lib/food_list_page.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:intl/intl.dart';
 import 'food_item_card.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
 
 class FoodListPage extends StatefulWidget {
   const FoodListPage({super.key});
@@ -12,15 +16,20 @@ class FoodListPage extends StatefulWidget {
 }
 
 class _FoodListPageState extends State<FoodListPage> {
-  late final List<List<dynamic>> _originalItems; // Store original list
-  List<List<dynamic>> filteredItems = [];
+  late final List<Map<String, dynamic>> _originalItems = [];
+  List<Map<String, dynamic>> filteredItems = [];
   Timer? _debounce;
   final TextEditingController _controller = TextEditingController();
-  List<String> _suggestions = [];
+  late final GenerativeModel aiModel;
+  final FocusNode _textFocusNode = FocusNode();
 
   @override
   void initState() {
     super.initState();
+    aiModel = GenerativeModel(
+        model: 'gemini-1.5-flash-latest',
+        apiKey: const String.fromEnvironment('api_key'));
+
     // Load and filter the CSV data
     loadAndFilterData();
 
@@ -35,11 +44,13 @@ class _FoodListPageState extends State<FoodListPage> {
 
   Future<void> loadAndFilterData() async {
     final rawData = await loadCSV();
-
-    // Ensure all items except the header row are displayed
+    final headers = rawData.first.map((e) => e.toString()).toList();
     setState(() {
-      _originalItems = rawData.skip(1).toList(); // Skip the first row
-      filteredItems = List.from(_originalItems); // Initialize with original list
+      _originalItems.clear();
+      _originalItems.addAll(rawData.skip(1).map((row) {
+        return Map<String, dynamic>.fromIterables(headers, row);
+      }).toList());
+      filteredItems = List.from(_originalItems);
     });
   }
 
@@ -49,11 +60,38 @@ class _FoodListPageState extends State<FoodListPage> {
     return csvTable;
   }
 
-  void _filterSearchResults(String query) {
-    final filtered = _originalItems.where((item) {
-      final lowerCaseQuery = query.toLowerCase();
+  void _filterSearchResults(Map<String, dynamic> geminiResponse) {
+    final details = geminiResponse['details'] ?? {};
 
-      return item.any((value) => value.toString().toLowerCase().contains(lowerCaseQuery));
+    final filtered = _originalItems.where((item) {
+      bool matchesAll = true;
+
+      details.forEach((key, value) {
+        if (value != null &&
+            value.toString().isNotEmpty &&
+            value.toString().toLowerCase() != 'n/a') {
+          final lowerCaseValue = value.toString().toLowerCase();
+
+          if (key == 'time') {
+            final itemTimeRange = item['Available Timing'];
+            final timeToCheck = lowerCaseValue== 'now'
+                ? DateFormat('h:mm a').format(DateTime.now()).toUpperCase() // Format current time if value is 'now'
+                : lowerCaseValue.toUpperCase();
+
+            if (!_isCurrentTimeWithinRange(timeToCheck, itemTimeRange)) {
+              matchesAll = false;
+            }
+          } else {
+            final itemToBeUsed = item[key];
+            final itemValue = itemToBeUsed?.toString().toLowerCase();
+            if (itemValue == null || !itemValue.contains(lowerCaseValue)) {
+              matchesAll = false;
+            }
+          }
+        }
+      });
+
+      return matchesAll;
     }).toList();
 
     setState(() {
@@ -61,19 +99,80 @@ class _FoodListPageState extends State<FoodListPage> {
     });
   }
 
+  bool _isCurrentTimeWithinRange(String currentTimeString, String timeRange) {
+    bool isInRange = false;
+
+    try {
+
+      final normalizedTimeRange = timeRange.replaceAll(RegExp(r'[\u00A0\u2007\u202F]'), ' ').trim();
+      final parts = normalizedTimeRange.split('-');
+      final startTimeString = parts[0].trim();
+      final endTimeString = parts[1].trim();
+
+      DateFormat formatter = DateFormat("h:mm a");
+
+      DateTime currentTime = formatter.parse(currentTimeString);
+      DateTime startTime = formatter.parse(startTimeString);
+      DateTime endTime = formatter.parse(endTimeString);
+
+      if (endTime.isBefore(startTime)) {
+        // Time range crosses midnight
+        isInRange = currentTime.isAfter(startTime) ||
+            currentTime.isBefore(endTime) ||
+            currentTime.isAtSameMomentAs(startTime) ||
+            currentTime.isAtSameMomentAs(endTime);} else {
+        // Normal time range
+        isInRange = currentTime.isAtSameMomentAs(startTime) ||
+            currentTime.isAtSameMomentAs(endTime) ||
+            (currentTime.isAfter(startTime) && currentTime.isBefore(endTime));
+      }
+
+    } catch (e) {
+      print('Error parsing time: $e');
+      return false; // Return false in case of parsing errors
+    }
+    return isInRange;
+  }
+
+  String normalizeSpaces(String input) {
+    return input.replaceAll(RegExp(r'[\u00A0\u2007\u202F]'), ' ').trim();
+  }
 
   void _onSearchChanged(String query) {
     if (_debounce?.isActive ?? false) _debounce!.cancel();
-    _debounce = Timer(const Duration(milliseconds: 300), () {
+    _debounce = Timer(const Duration(milliseconds: 300), () async {
       if (query.isNotEmpty) {
-        _filterSearchResults(query);
+        final intentData = await getGeminiIntent(query);
+        _filterSearchResults(intentData);
       } else {
         setState(() {
-          filteredItems = List.from(_originalItems); // Reset to original list
-          _suggestions = [];
+          filteredItems = List.from(_originalItems);
         });
       }
     });
+  }
+
+  Future<Map<String, dynamic>> getGeminiIntent(String query) async {
+    try {
+      // Updated prompt message
+      final prompt =
+          'Given the user query: "$query", generate a structured JSON response that represents the intent and relevant details for searching food items. Please ensure the JSON is in the format {"intent": "search_items", "details": {"Item Name": "string","Category": "string", "Location": "string", "time": "string"}}. If a detail like "Item Name", "Category", "Location", or "time" is not specified in the user query, set its value to null. The JSON should only contain valid key-value pairs with no additional formatting or special characters.';
+
+      final content = [Content.text(prompt)];
+      final response = await aiModel.generateContent(content);
+      var jsonString = response.candidates.first.text.toString().trim();
+
+      // Clean up the response
+      jsonString =
+          jsonString.replaceAll('```json', '').replaceAll('```', '').trim();
+
+      // Attempt to parse the JSON
+      final decodedJson = jsonDecode(jsonString);
+      return decodedJson is Map<String, dynamic> ? decodedJson : {};
+    } catch (e) {
+      print('Error getting intent: $e');
+      return {};
+    }
   }
 
   @override
@@ -91,25 +190,26 @@ class _FoodListPageState extends State<FoodListPage> {
                 Expanded(
                   child: TextField(
                     controller: _controller,
+                    focusNode: _textFocusNode,
                     decoration: InputDecoration(
-                      hintText: 'Search food items...',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      prefixIcon: const Icon(Icons.search),
-                      suffixIcon: _controller.text.isNotEmpty // Show clear button if text is present
-                          ? IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: () {
-                          _controller.clear(); // Clear the text field
-                          setState(() {
-                            filteredItems = List.from(_originalItems); // Reset the filtered list
-                          });
-                        },
-                      )
-                          : null,
-                    ),
-                    onChanged: (query) { // Trigger search on text change
+                        hintText: 'Search food items...',
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        prefixIcon: const Icon(Icons.search),
+                        suffixIcon: IconButton(
+                          icon: const Icon(Icons.clear),
+                          onPressed: () {
+                            _controller.clear();
+                            setState(() {
+                              filteredItems = List.from(
+                                  _originalItems); // Reset the filtered list
+                            });
+                          },
+                        )),
+                    textInputAction: TextInputAction.done,
+                    onSubmitted: (query) {
+                      _textFocusNode.unfocus();
                       _onSearchChanged(query);
                     },
                   ),
@@ -128,10 +228,10 @@ class _FoodListPageState extends State<FoodListPage> {
                 itemBuilder: (context, index) {
                   final item = filteredItems[index];
                   return FoodItemCard(
-                    itemName: item[0],
-                    category: item[1],
-                    timing: item[2],
-                    location: item[3],
+                    itemName: item['Item Name'],
+                    category: item['Category'],
+                    timing: item['Available Timing'],
+                    location: item['Location'],
                     onAddToCart: () {
                       // Handle add to cart action here
                     },
